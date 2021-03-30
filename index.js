db5f04448a3e1e96f376aaa2519314f8cc14fb26
Abirk2day <abirk2day@gmail.com>
2021-03-30 19:14:53 +0600

    nodejs

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
-const express = require('express');
 
-  
-  
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId
 
 
 
-app.get('/',  (req, res) => {
-    res.send('hello world Thank you for calling me Jhanker Vi is grade')
+
+
+const uri = "mongodb+srv://userDocu:yDZXnzRcpwNj9Gqw@cluster0.qatfi.mongodb.net/documentUser?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const app = express()
+const port = 5000
+
+const pass = 'yDZXnzRcpwNj9Gqw';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+client.connect(err => {
+  const productCollection = client.db("documentUser").collection("userss");
+
+  app.get('/products', (req, res) => {
+    productCollection.find({})
+      .toArray((err, document) => {
+        res.send(document)
+      })
   })
 
-app.listen(4000, () => console.log('Listening to port 4000'))
+  app.get("/product/:id", (req, res) => {
+    productCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, document) => {
+        res.send(document[0])
+      })
+  })
+
+
+  app.patch(`/update/:id`, (req,  res) =>{
+    console.log(req.params.id)
+    productCollection.updateOne({ _id: ObjectId(req.params.id) },
+    
+    {
+      $set: {price: req.body.price, quantity: req.body.quantity}
+    }
+    
+    )
+    
+   .then(result =>{
+     res.send(result.modifiedCount > 0)
+   })
+
+  })
+
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    productCollection.insertOne(product)
+      .then(result => {
+        console.log("success")
+      })
+    res.redirect("/")
+  })
+
+
+  app.delete('/delete/:id', (req, res) => {
+    productCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => {
+         res.send(result.deletedCount > 0)
+      })
+
+
+  
+  })
+
+});
+
+
+
+app.listen(port)
